b47fbb0bd1604ca9a7a2ac7ac239a13c45afdcd5
Thiago Giammattey <giammattey@gmail.com>
2020-02-05 23:09:17 -0300

    adiciona funcionalidade de maximizar modal

--- public/scripts.js
+++ public/scripts.js
@@ -1,4 +1,5 @@
 const modalOverlay = document.querySelector('.modal-overlay')
+const modal = document.querySelector('.modal')
 const cards = document.querySelectorAll('.card')
 const iframeEl = document.querySelector('iframe')
 
@@ -8,16 +9,47 @@ for (let card of cards) {
 			modalOverlay.classList.add('active')
 			iframeEl.src = `https://rocketseat.com.br/${card.dataset.course}`
 		}
+
+		if (modal.classList.contains('maximize')) {
+			modal.classList.remove('maximize')
+		}
 	})
+}
+
+function maximizeModal() {
+	if (modal.classList.contains('maximize')) {
+		return modal.requestFullscreen()
+	} else {
+		modal.classList.remove('maximize')
+		return document.exitFullscreen()
+	}
 }
 
 document.addEventListener('click', function(e) {
 	if (e.target.matches('#close-modal') || e.target.matches('.modal-overlay')) {
 		iframeEl.src = ''
 		modalOverlay.classList.remove('active')
+
+		modal.classList.remove('maximize')
+		maximizeModal()
+		return
+	}
+
+	if (e.target.closest('#maximize-modal')) {
+		if (!modal.classList.contains('maximize')) {
+			modal.classList.add('maximize')
+		} else {
+			modal.classList.remove('maximize')
+			return maximizeModal()
+		}
+		return maximizeModal()
 	}
 })
 
-// utilize a mesma lógica implementada para fechar o modalOverlay, mas trabalhe com a classe modal e utilize o método contains do classList para verificar se o elemento está ou não com a classe maximize
-
-// O modal nunca deve abrir maximizado
+document.addEventListener('fullscreenchange', e => {
+	if (!document.fullscreenElement) {
+		modal.classList.remove('maximize')
+		maximizeModal()
+		return
+	}
+})
